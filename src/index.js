cc608cf2ecd6c2cc56f1f806275107f711b9cbc2
MaxiStarovoitov <rackezik@gmail.com>
2018-02-26 17:17:07 +0300

    Solution. Task 3 .Love-triangle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var lovesTriangles = 0,
+    lovesArr = preferences,
+    first,
+    second,
+    three;
+
+  preferences.forEach(function(item, i, preferences) {
+    first = i+1;
+    second = preferences[i];
+    three = preferences[second-1];
+
+    if (lovesArr[first-1] != undefined ||
+      lovesArr[second-1] != undefined ||
+      lovesArr[three-1] != undefined) {
+
+      if(preferences[three-1] === first && first != second) {
+        lovesArr[first - 1] = undefined;
+        lovesArr[second - 1] = undefined;
+        lovesArr[three - 1] = undefined;
+
+        lovesTriangles++;
+      }
+    }
+  });
+
+  return lovesTriangles;
 };
